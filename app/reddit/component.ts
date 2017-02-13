94caadad4295dddcfa8137175c0017d889c2f8f6
idlviv <idlviv@rambler.ru>
2017-02-13 21:05:38 +0200

    13/02/17 home

--- app/reddit/component.ts
+++ app/reddit/component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'reddit',
+  styleUrls: ['./app/reddit/reddit.css'],
   template: `
     <form class="ui large form segment">
         <h3 class="ui header">Добавити ще посилання</h3>
-        
         <div class="field">
             <label for="title">Заголовок</label>
             <input type="text" name="title">
@@ -14,10 +14,14 @@ import { Component } from '@angular/core';
             <label for="link">Посилання</label>
             <input type="text" name="link">
         </div>
-        <button class="ui positive right floated button">
+        <button class="ui positive right floated button" (click)="addArticle($event)">
             Відправити
         </button>
      </form>
   `
 })
-export class RedditAppComponent {}
+export class RedditAppComponent {
+  addArticle(event: any) {
+    console.log('pressed' + event);
+  }
+}
